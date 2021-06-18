e5958c90fc3abf97de42530f4ec57a9104f89c54
Marco Tessitore <marcotexcode@gmail.com>
2021-06-18 16:19:52 +0200

    add sistemazione layout

--- js/script.js
+++ js/script.js
@@ -1,123 +1,90 @@
 
 const icons = [
-    {
-      name: 'cat',
-      prefix: 'fa-cat',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'crow',
-      prefix: 'fa-crow',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dog',
-      prefix: 'fa-dog',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dove',
-      prefix: 'fa-dove',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dragon',
-      prefix: 'fa-dragon',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'horse',
-      prefix: 'fa-horse',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'hippo',
-      prefix: 'fa-hippo',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'fish',
-      prefix: 'fa-fish',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'carrot',
-      prefix: 'fa-carrot',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'apple-alt',
-      prefix: 'fa-apple-alt',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'lemon',
-      prefix: 'fa-lemon',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'pepper-hot',
-      prefix: 'fa-pepper-hot',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'user-astronaut',
-      prefix: 'fa-user-astronaut',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-graduate',
-      prefix: 'fa-user-graduate',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-ninja',
-      prefix: 'fa-user-ninja',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-secret',
-      prefix: 'fa-user-secret',
-      type: 'user',
-      family: 'fas',
-    },
+
+  {name:'cat', prefix:'fa-cat', type:'animal', family:'fas'}, 
+  
+  {name:'crow', prefix:'fa-crow', type:'animal', family:'fas', }, 
+  
+  {name:'dog', prefix:'fa-dog', type:'animal', family:'fas', }, 
+  
+  {name:'dove', prefix:'fa-dove', type:'animal', family:'fas', }, 
+  
+  {name:'dragon', prefix:'fa-dragon', type:'animal', family:'fas', }, 
+  
+  {name:'horse', prefix:'fa-horse', type:'animal', family:'fas', }, 
+  
+  {name:'hippo', prefix:'fa-hippo', type:'animal', family:'fas', }, 
+  
+  {name:'fish', prefix:'fa-fish', type:'animal', family:'fas', }, 
+  
+  {name:'carrot', prefix:'fa-carrot', type:'vegetable', family:'fas', }, 
+  
+  {name:'apple-alt', prefix:'fa-apple-alt', type:'vegetable', family:'fas', }, 
+  
+  {name:'lemon', prefix:'fa-lemon', type:'vegetable', family:'fas', }, 
+  
+  {name:'pepper-hot', prefix:'fa-pepper-hot', type:'vegetable', family:'fas', }, 
+  
+  {name:'user-astronaut', prefix:'fa-user-astronaut', type:'user', family:'fas', }, 
+  
+  {name:'user-graduate', prefix:'fa-user-graduate', type:'user', family:'fas', }, 
+  
+  {name:'user-ninja', prefix:'fa-user-ninja', type:'user', family:'fas', }, 
+  
+  {name:'user-secret', prefix:'fa-user-secret', type:'user', family:'fas', }, 
+
   ];
+
 
 print(icons);
 
-const colorAnimal = 'red';
-const colorUser = '';
-const colorVegetable = '';
+// const coloredArray = icons.map((element) => {
+
+//   let color = 'purple';
+
+//   const {name, prefix, type, family} = element;
+
+  
+//   if(type == 'animal') {
+//     color = 'blue';
+//   } else if (type == 'vegetable') {
+//     color = 'orange';
+//   } 
+  
+
+//   return {
+//     name, 
+//     prefix, 
+//     type, 
+//     family,
+//     color
+//   }
+
+// });
 
 
-const addColor = icons.map((element) => {
-
-  const {name, prefix, type, family} = element;
-
-  if (type === 'user') {
-    color = colorAnimal;
-  }
 
 
-  return {
-   color
-  }
-});
+
+
+// const colorAnimal = 'red';
+// const colorUser = '';
+// const colorVegetable = '';
+
+
+// const addColor = icons.map((element) => {
+
+//   const {name, prefix, type, family} = element;
+
+//   if (type == 'user') {
+//     color : 'red';
+//   }
+
+
+//   return {
+//    color
+//   }
+// });
 
 
 
@@ -133,9 +100,9 @@ function print(array) {
     document.getElementById('icons').innerHTML += 
 
     `
-        <div class="box1">
-            <span><i class="${prefix} ${family}"></i><p>${name}</p></span>
-        </div>
+      <div class="box1 p-5 col-2 m-3  text-center">
+          <span><i class="${prefix} ${family}"></i><p>${name}</p></span>
+      </div>
         
     `
 
